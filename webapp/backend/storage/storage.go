2fb84614949de9d9faa9a46bbae876d1aa6fea80
silly-portugeese <87879934+silly-portugeese@users.noreply.github.com>
2024-08-07 12:14:59 +0100

    Update data layer to use channels instead of Mutex

--- webapp/backend/storage/storage.go
+++ webapp/backend/storage/storage.go
@@ -2,10 +2,15 @@ package storage
 
 import (
 	"errors"
+	"fmt"
 	"slices"
-	"sync"
 	"todo-webapp/backend/models"
 )
+
+// Understanding Channel Types
+// Send-Only Channel (chan<- T): This type of channel can only be used to send data.
+// Receive-Only Channel (<-chan T): This type of channel can only be used to receive data.
+// Bidirectional Channel (chan T): This type of channel can be used for both sending and receiving data.
 
 type IToDoRepository interface {
 	FindAll() []models.ToDo
@@ -16,13 +21,25 @@ type IToDoRepository interface {
 }
 
 // Implementation
-type ToDoStoreImpl struct {
-	mu sync.Mutex
+type toDoStoreImpl struct {
+	cmds   chan command
 	items  []models.ToDo
 	nextId int
 }
 
-func NewInMemoryStore() ToDoStoreImpl {
+type response struct {
+	item  models.ToDo
+	items []models.ToDo
+	err   error
+}
+
+type command struct {
+	action string
+	args   map[string]interface{}
+	reply  chan response
+}
+
+func NewInMemoryStore() toDoStoreImpl {
 
 	list := []models.ToDo{
 		{Id: 1, Task: "Buy groceries", Status: "Pending"},
@@ -36,45 +53,95 @@ func NewInMemoryStore() ToDoStoreImpl {
 		{Id: 9, Task: "Plan vacation", Status: "Pending"},
 		{Id: 10, Task: "Learn Go", Status: "In Progress"},
 	}
-	return ToDoStoreImpl{items: list, nextId: len(list) + 1}
 
-	// return ToDoStoreImpl{items: []models.ToDo{}, nextId: 1}
+	store := toDoStoreImpl{items: list, nextId: len(list) + 1, cmds: make(chan command)}
+	go store.launchRequestManager()
+	return store
+
 }
 
-func NewEmptyInMemoryStore() ToDoStoreImpl {
-	return ToDoStoreImpl{items: []models.ToDo{}, nextId: 1}
+func NewEmptyInMemoryStore() toDoStoreImpl {
+	store := toDoStoreImpl{items: []models.ToDo{}, nextId: 1, cmds: make(chan command)}
+	go store.launchRequestManager()
+	return store
 }
 
-func (tds *ToDoStoreImpl) FindAll() []models.ToDo {
-	return tds.items
+func (tds *toDoStoreImpl) launchRequestManager() {
+
+	executers := map[string]func(cmd command){
+		"create": tds.executeCreate,
+		"read":   tds.executeFindById,
+		"update": tds.executeUpdate,
+		"delete": tds.executeDelete,
+		"list":   tds.executeFindAll,
+	}
+
+	for cmd := range tds.cmds {
+		execute, ok := executers[cmd.action]
+		if !ok {
+			cmd.reply <- response{err: fmt.Errorf("unknown action: %s", cmd.action)}
+			continue
+		}
+		execute(cmd)
+	}
 }
 
-func (tds *ToDoStoreImpl) FindById(id int) (models.ToDo, error) {
+// --- Internal Methods ---
+// These methods are used internally by the RequestManager to handle specific commands: create, read, update, delete, list
+
+func (tds *toDoStoreImpl) executeFindAll(cmd command) {
+	cmd.reply <- response{items: tds.items}
+}
+
+func (tds *toDoStoreImpl) executeFindById(cmd command) {
+
+	id, hasId := cmd.args["id"].(int)
+
+	if !hasId {
+		cmd.reply <- response{item: models.ToDo{}, err: fmt.Errorf("missing id: %d", id)}
+		return
+	}
+
 	index := tds.getItemIndex(id)
 	if index < 0 {
-		return models.ToDo{}, errors.New("to do item not found")
+		cmd.reply <- response{item: models.ToDo{}, err: errors.New("to do item not found")}
+		return
 	}
-	return tds.items[index], nil
+
+	cmd.reply <- response{item: tds.items[index], err: nil}
 }
 
-func (tds *ToDoStoreImpl) Create(task string, status models.Status) models.ToDo {
-	tds.mu.Lock()
-	defer tds.mu.Unlock()
-	
+func (tds *toDoStoreImpl) executeCreate(cmd command) {
+
+	task, hasTask := cmd.args["task"].(string)
+	status, hasStatus := cmd.args["status"].(models.Status)
+
+	if !hasTask || !hasStatus {
+		cmd.reply <- response{err: errors.New("missing arguments")}
+		return
+	}
+
 	item := models.ToDo{Id: tds.nextId, Task: task, Status: status}
 	tds.items = append(tds.items, item)
-	tds.nextId += 1
-	return item
+	tds.nextId++
+	cmd.reply <- response{item: item}
 }
 
-func (tds *ToDoStoreImpl) Update(id int, task *string, status *models.Status) (models.ToDo, error) {
-	tds.mu.Lock()
-	defer tds.mu.Unlock()
+func (tds *toDoStoreImpl) executeUpdate(cmd command) {
+
+	id, hasId := cmd.args["id"].(int)
+	task, _ := cmd.args["task"].(*string)
+	status, _ := cmd.args["status"].(*models.Status)
+
+	if !hasId {
+		cmd.reply <- response{item: models.ToDo{}, err: fmt.Errorf("missing id: %d", id)}
+		return
+	}
 
 	index := tds.getItemIndex(id)
-
 	if index < 0 {
-		return models.ToDo{}, errors.New("to do item not found")
+		cmd.reply <- response{item: models.ToDo{}, err: errors.New("to do item not found")}
+		return
 	}
 
 	// simulate optional parameters by using pointers.
@@ -86,24 +153,66 @@ func (tds *ToDoStoreImpl) Update(id int, task *string, status *models.Status) (m
 		tds.items[index].Status = *status
 	}
 
-	return tds.items[index], nil
-
+	cmd.reply <- response{item: tds.items[index], err: nil}
 }
 
-func (tds *ToDoStoreImpl) Delete(id int) error {
-	tds.mu.Lock()
-	defer tds.mu.Unlock()
+func (tds *toDoStoreImpl) executeDelete(cmd command) {
+
+	id, hasId := cmd.args["id"].(int)
+	if !hasId {
+		cmd.reply <- response{err: fmt.Errorf("missing id: %d", id)}
+		return
+	}
 
 	index := tds.getItemIndex(id)
-
 	if index < 0 {
-		return errors.New("to do item not found")
+		cmd.reply <- response{err: errors.New("to do item not found")}
+		return
 	}
 
 	tds.items = append(tds.items[:index], tds.items[index+1:]...)
-	return nil
+	cmd.reply <- response{err: nil}
 }
 
-func (tds *ToDoStoreImpl) getItemIndex(id int) int {
+func (tds *toDoStoreImpl) getItemIndex(id int) int {
 	return slices.IndexFunc(tds.items, func(td models.ToDo) bool { return td.Id == id })
+}
+
+// --- Interface Methods ---
+// They are exposed to the rest of the application.
+
+func (tds *toDoStoreImpl) FindAll() []models.ToDo {
+	ch := make(chan response)
+	tds.cmds <- command{action: "list", reply: ch}
+	response := <-ch
+	return response.items
+}
+
+func (tds *toDoStoreImpl) FindById(id int) (models.ToDo, error) {
+	ch := make(chan response)
+	tds.cmds <- command{action: "read", reply: ch, args: map[string]interface{}{"id": id}}
+	response := <-ch
+	return response.item, response.err
+}
+
+func (tds *toDoStoreImpl) Create(task string, status models.Status) models.ToDo {
+	ch := make(chan response)
+	tds.cmds <- command{action: "create", reply: ch, args: map[string]interface{}{"task": task, "status": status}}
+	response := <-ch
+	return response.item
+}
+
+func (tds *toDoStoreImpl) Update(id int, task *string, status *models.Status) (models.ToDo, error) {
+	ch := make(chan response)
+	tds.cmds <- command{action: "update", reply: ch, args: map[string]interface{}{"id": id, "task": task, "status": status}}
+	response := <-ch
+	return response.item, response.err
+
+}
+
+func (tds *toDoStoreImpl) Delete(id int) error {
+	ch := make(chan response)
+	tds.cmds <- command{action: "delete", reply: ch, args: map[string]interface{}{"id": id}}
+	response := <-ch
+	return response.err
 }
