7eeefb0ac00ad0ef92c0ff5608da1b258ffa43e7
silly-portugeese <87879934+silly-portugeese@users.noreply.github.com>
2024-08-08 10:52:05 +0100

    Improve cli app functionality

--- cli_app/main.go
+++ cli_app/main.go
@@ -6,20 +6,27 @@ import (
 	"fmt"
 	"os"
 	"slices"
+	"strconv"
 	"strings"
 )
+
+var IdCounter int
 
 type ToDoList struct {
 	Items []ToDo `json:"todolist"`
 }
 
 type ToDo struct {
+	Id int `json:"id"`
 	Task string `json:"task"`
 	Status string `json:"status"`
 }
 
 func NewToDo(name string) ToDo {
+	IdCounter++
+
 	return ToDo{
+		Id: IdCounter,
 		Task: name,
 		Status:  "Pending",
 	}
@@ -43,17 +50,35 @@ func (tdl *ToDoList) List() ([]ToDo) {
 	return tdl.Items
 }
 
-func (tdl *ToDoList) Update(task string, s string) (error) {
-	index := slices.IndexFunc(tdl.Items, func(td ToDo) bool { return td.Task == task })
+func (tdl *ToDoList) Read(id int) (ToDo, error) {
+	index := slices.IndexFunc(tdl.Items, func(td ToDo) bool { return td.Id == id})
+	if index < 0 {
+		return ToDo{}, errors.New("Task does not exist")
+	}
+	return tdl.Items[index], nil
+}
+
+func (tdl *ToDoList) Update(id int, name string, status string) (error) {
+	index := slices.IndexFunc(tdl.Items, func(td ToDo) bool { return td.Id == id})
 	if index < 0 {
 		return errors.New("Task does not exist")
 	}
-	tdl.Items[index].Status = s
+
+	if name == "" && status == "" {
+		return errors.New("Nothing to update")
+	}
+
+	if name != "" {
+		tdl.Items[index].Task = name
+	}
+	if status != "" {
+		tdl.Items[index].Status = status
+	}
 	return nil
 }
 
-func (tdl *ToDoList) Delete(task string) error {
-	index := slices.IndexFunc(tdl.Items, func(td ToDo) bool { return td.Task == task })
+func (tdl *ToDoList) Delete(id int) error {
+	index := slices.IndexFunc(tdl.Items, func(td ToDo) bool { return td.Id == id })
 	if index < 0 {
 		return errors.New("Task does not exist")
 	}
@@ -68,6 +93,10 @@ func readCmd(reader bufio.Reader, msg string) (s string) {
 	return input
 }
 
+func strToInt(s string) {
+
+}
+
 func main() {
 	// In memory To Do app
 	reader := bufio.NewReader(os.Stdin)
@@ -75,7 +104,7 @@ func main() {
 	
 	todos := ToDoList{}
 	for {
-		fmt.Println("Choose an operation: create, read, update, delete, or exit")
+		fmt.Println("Choose an operation: create (c), list (l), read (r), update (u), delete (d), or exit (e)")
 		fmt.Print("> ")
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimSpace(input)
@@ -83,37 +112,79 @@ func main() {
 
 		
 		switch input {
-		case "create":
+		case "create", "c":
 			input = readCmd(*reader, "Enter a task: ")
 			todos.Create(input)
-			fmt.Println("task created")
-		case "read":
+			fmt.Println("Task created")
+		
+		case "list", "l":
 			var list []ToDo
 			list = todos.List()
 			for _,i := range list {
-				fmt.Printf("Task: %s; Status: %s\n", i.Task, i.Status)
+				fmt.Printf("ID: %d; Task: %s; Status: %s\n", i.Id, i.Task, i.Status)
+			}
+
+		case "read", "r":
+			idStr := readCmd(*reader, "Give task ID: ")
+
+			id, err := strconv.Atoi(idStr); 
+			if err != nil {
+				fmt.Println("Invalid ID")
+				continue
+			} 
+			task, err := todos.Read(id)
+			if err != nil {
+				fmt.Println(err.Error())
+			} else {
+				fmt.Printf("ID: %d; Task: %s; Status: %s\n", task.Id, task.Task, task.Status)
 			}
 			
-		case "update":
-			task := readCmd(*reader, "Which task to update? ")
-			status := readCmd(*reader, "Update task status to: ")
-			err := todos.Update(task, status)
+			
+		case "update", "u":
+			idStr := readCmd(*reader, "Which task to update? Give task ID: ")
+			id, err := strconv.Atoi(idStr)
 			if err != nil {
-				println(err)
-			} else {
-				println("Task updated")
+				fmt.Println("Invalid ID")
+				continue
 			}
-		case "delete":
-			task := readCmd(*reader, "Which task to delete? ")
-			err := todos.Delete(task)
+			
+			_, err = todos.Read(id)
 			if err != nil {
-				println(err)
+				fmt.Println(err.Error())
+				continue
+			} 
+
+			task := readCmd(*reader, "Update task name to... If you don't want to update it, leave it empty: ")
+			status := readCmd(*reader, "Update task status to... If you don't want to update it, leave it empty: ")
+
+			err = todos.Update(id, task, status)
+			if err != nil {
+				fmt.Println(err.Error())
 			} else {
-				println("Task deleted")
+				fmt.Println("Task updated")
 			}
-		case "exit":
+		
+		case "delete", "d":
+			idStr := readCmd(*reader, "Which task to delete? Give task ID: ")
+			id, err := strconv.Atoi(idStr); 
+			
+			if err != nil {
+				fmt.Println("Invalid ID")
+				continue
+			}
+
+			err = todos.Delete(id)
+			if err != nil {
+				fmt.Println(err.Error())
+				
+			} else {
+				fmt.Println("Task deleted")
+			}
+		
+		case "exit", "e":
 			fmt.Println("Exiting...")
 			return
+		
 		default:
 			fmt.Println("Unknown operation")
 		}
